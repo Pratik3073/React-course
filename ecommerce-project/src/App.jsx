a5a503e3e4b37e97188367e9debd91e9e501f89a
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-11 12:50:10 +0530

    Update routing and dependencies in the Ecommerce project. Added react-router-dom for improved navigation and integrated OrdersPage component into the routing structure. Updated package.json and package-lock.json to include react-router-dom as a dependency. Removed outdated orders.html and associated styles to streamline the project.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { CheckoutPage } from './Pages/CheckoutPage'
 import { HomePage } from './Pages/HomePage'
 import { Routes,Route } from 'react-router'
+import { OrdersPage } from './Pages/OrdersPage'
 
 function App() {
  
@@ -10,7 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage />} /> 
       <Route path='checkout' element={<CheckoutPage />} />
-     
+      <Route path='orders' element={<OrdersPage />} />
     </Routes>
   )
 }
