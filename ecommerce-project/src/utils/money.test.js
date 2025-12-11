3cbfe9847b40656cffb9f188c278130a6f84e1ca
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-11 21:40:16 +0530

    Update package.json and package-lock.json to include vitest dependency and related packages. This enhances testing capabilities within the project by integrating Vitest and its associated modules.

---
+++ ecommerce-project/src/utils/money.test.js
@@ -0,0 +1,19 @@
+import { it,expect,describe } from "vitest";//describe = groups test together
+import { formatMoney } from "./money";
+
+describe('formatMoney',()=>{ // 'formatMoney' test suite name
+    it('fotmats 1999 sents as $19.99',()=>{
+        expect(formatMoney(1999)).toBe('$19.99');   // .toBe is method 
+    });
+    
+    it('displays 2 decimals',()=>{
+        expect(formatMoney(1090)).toBe('$10.90');
+        expect(formatMoney(100)).toBe('$1.00')
+    });
+});
+
+
+
+
+
+// to run vitest - npm vitest
