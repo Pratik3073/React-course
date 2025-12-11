5c4804900eb64d0e21aeb8b73c0e364588d41613
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-11 11:06:56 +0530

    Update package dependencies and implement routing in App component. Added react-router for navigation and integrated basic routes for HomePage and a test checkout page. Updated package.json and package-lock.json to include react-router as a dependency.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 import { HomePage } from './Pages/HomePage'
+import { Routes,Route } from 'react-router'
+
 function App() {
  
 
   return (
-   <HomePage />
+    <Routes>
+      <Route path='/' element={<HomePage />} /> 
+      <Route path='checkout' element={<div>test page</div>} />
+     
+    </Routes>
   )
 }
 
