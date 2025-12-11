41c2537ca3132b37d7f187d476a248e5d545697a
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-11 13:00:47 +0530

    Refactor header implementation by removing header.css and integrating Header component in HomePage and OrdersPage. This change streamlines the code and enhances maintainability by centralizing header styles and functionality.

--- ecommerce-project/src/Pages/OrdersPage.jsx
+++ ecommerce-project/src/Pages/OrdersPage.jsx
@@ -1,4 +1,4 @@
-import './header.css'
+import { Header } from '../components/Header';
 import './Orders.css'
 import { Link } from "react-router-dom";
 
@@ -7,33 +7,7 @@ export function OrdersPage() {
     <>
       <title>Orders</title>
 
-      <div className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
-      </div>
+       <Header />
 
       {/* PAGE CONTENT */}
       <div className="orders-page">
