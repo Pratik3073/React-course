840b12e28974c7d0f67b47998a64455966f1cc6d
Pratik3073 <102535841+Pratik3073@users.noreply.github.com>
2025-12-12 09:49:22 +0530

    Refactor Product tests to improve structure and readability. Introduced beforeEach for setup, ensuring consistent test conditions. Enhanced product detail verification and maintained existing add-to-cart functionality tests.

--- ecommerce-project/src/Pages/home/Product.test.jsx
+++ ecommerce-project/src/Pages/home/Product.test.jsx
@@ -1,14 +1,17 @@
-import { it, expect, describe, vi } from "vitest"; //describe = groups test together
+import { it, expect, describe, vi, beforeEach } from "vitest"; //describe = groups test together
 import { Product } from "./Product";
 import { render, screen } from "@testing-library/react"; // renders a component in a fake web page
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event";
 import axios from "axios";
 
-vi.mock('axios'); // fake version of axios
+vi.mock("axios"); // fake version of axios
 
 describe("Product Component", () => {
-  it("displays the products detals correctly", () => {
-    const product = {
+  let product ; 
+  let loadCart; 
+ 
+ beforeEach(()=>{
+    product={
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,66 +22,47 @@ describe("Product Component", () => {
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"],
     };
-    const loadCart = vi.fn(); // vi.fn() = create fake function that doesnt do anything
+    loadCart=vi.fn(); // vi.fn() = create fake function that doesnt do anything
+  });
+
+  it("displays the products detals correctly", () => {
     render(<Product product={product} loadCart={loadCart} />);
-    
+
     expect(
       screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs") //it search the element on the screen
     ).toBeInTheDocument(); //to check this element is available on screen on the fake web page
-    
-    expect(
-        screen.getByText('$10.90')
-    ).toBeInTheDocument();
 
-    expect(
-        screen.getByTestId("product-image")
-    ).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg');
+    expect(screen.getByText("$10.90")).toBeInTheDocument();
 
-    expect(
-        screen.getByTestId('product-rating-stars-image')
-      ).toHaveAttribute(
-        'src',
-        `images/ratings/rating-45.png`
-      );
+    expect(screen.getByTestId("product-image")).toHaveAttribute(
+      "src",
+      "images/products/athletic-cotton-socks-6-pairs.jpg"
+    );
 
-      expect(
-        screen.getByText('87')
-      ).toBeInTheDocument();   
-});
+    expect(screen.getByTestId("product-rating-stars-image")).toHaveAttribute(
+      "src",
+      `images/ratings/rating-45.png`
+    );
 
-  it('adds a products to cart',async ()=>{
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-    const loadCart = vi.fn(); // vi.fn() = create fake function that doesnt do anything
-    render(<Product product={product} loadCart={loadCart} />); 
-    
+    expect(screen.getByText("87")).toBeInTheDocument();
+  });
+
+  it("adds a products to cart", async () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
     const user = userEvent.setup();
     const addToCartButton = screen.getByTestId("add-to-cart-button");
     await user.click(addToCartButton);
 
-    expect(axios.post).toHaveBeenCalledWith(
-      '/api/cart-items',
-      {
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:1
-      }
-    );
+    expect(axios.post).toHaveBeenCalledWith("/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+    });
     expect(loadCart).toHaveBeenCalled();
   });
-
 });
 // to button click function @testing-library/user-event
 // lets us simulate events (like clicking button)
-
 
 // mock = create fake version of this function
 // for testing the function we run the function
